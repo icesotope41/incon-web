b7ff35a2578d61d2adb8b5e4e303d2b034a2f2c6
Issac <53615543+icesotope41@users.noreply.github.com>
2021-11-25 17:35:54 +0800

    adding stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-      <h1 class="greeting">Hello!</h1>
-      <h2 class="greeting">This is in testing phase.</h2>
-      <div class="introText">
-        <p>There remains to have many things to come in the future.</p>
-  
-      </div>
+
+    let hidMsg = "in ten years maybe"
+
+    let visible = (id) => {
+        var d = document.getElementById(id);
+
+        if (d.style.color == "black") {
+            d.style.color =  "rgba(255, 140, 0, 0)";
+        } else {
+            d.style.color = "black";
+        }
+        
+
+    }
+
+    return (
+        <div className="App">
+        
+            <h1 className="greeting">Hello!</h1>
+            <h2 className="greeting">This is in testing phase.</h2>
+            <div className="introText">
+                <p>There remains to have many things to come in the future.</p>
+                <p className="noselect" id="reveal" onClick={() => visible("reveal")}>{hidMsg}</p>
+            </div>
       
 
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
